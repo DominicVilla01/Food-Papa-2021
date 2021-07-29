1168ab15baa1909044b960cda732ac0a818b54d9
GDivino <68019100+GDivino@users.noreply.github.com>
2021-07-29 21:30:21 +0800

    Added Test activity

---
+++ app/src/main/java/com/example/foodpapa2021/Test.java
@@ -0,0 +1,14 @@
+package com.example.foodpapa2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
