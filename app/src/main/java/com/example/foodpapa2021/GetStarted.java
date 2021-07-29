50abe9077b5bb3361a25d44960d2b722a90c640c
DominicVilla01 <dominic.villa@obf.ateneo.edu>
2021-07-29 20:59:58 +0800

    initial commit

---
+++ app/src/main/java/com/example/foodpapa2021/GetStarted.java
@@ -0,0 +1,30 @@
+package com.example.foodpapa2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_get_started)
+public class GetStarted extends AppCompatActivity {
+
+    @ViewById
+    Button getStarted;
+
+    @Click
+    public void getStarted()
+    {
+        LoginPage_.intent(this).start();
+    }
+
+    @AfterViews
+    public void init()
+    {
+
+    }
+}
---
+++ app/src/main/java/com/example/foodpapa2021/LoginPage.java
@@ -0,0 +1,12 @@
+package com.example.foodpapa2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_login_page)
+public class LoginPage extends AppCompatActivity {
+
+}
