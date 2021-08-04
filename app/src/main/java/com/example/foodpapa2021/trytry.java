9b4bd71f45b7cf6c24b8ea8b3c67345c8d34588b
danielbayta <dennisdanielbayta@gmail.com>
2021-08-05 01:31:31 +0800

    trytry
    trytry

---
+++ app/src/main/java/com/example/foodpapa2021/trytry.java
@@ -0,0 +1,14 @@
+package com.example.foodpapa2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trytry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trytry);
+    }
+}
