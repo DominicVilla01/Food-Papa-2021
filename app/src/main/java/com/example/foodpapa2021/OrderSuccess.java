e71514f8e7d6909783218cee80083e727ecba5da
GDivino <68019100+GDivino@users.noreply.github.com>
2021-08-05 22:25:19 +0800

    Add order success page

--- app/src/main/java/com/example/foodpapa2021/OrderSuccess.java
+++ app/src/main/java/com/example/foodpapa2021/OrderSuccess.java
@@ -3,10 +3,35 @@ package com.example.foodpapa2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_order_success)
 public class OrderSuccess extends AppCompatActivity {
+//    Set Vars
 
+
+//    Get Views
+    @ViewById
+    Button order_success_logout;
+
+    @ViewById
+    Button order_success_reorder;
+
+
+//    Function for reorder
+    public void order_success_reorder()
+    {
+        FoodDetail_.intent(this).start();
+    }
+
+
+//    Function for logout
+    public void order_success_logout()
+    {
+//        If cart is open close it
+        
+    }
 }
