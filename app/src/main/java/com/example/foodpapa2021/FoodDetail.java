f4d484bb1004447014f803838ed8c2c98f511a5e
alyssago <denise.go@obf.ateneo.edu>
2021-08-07 01:26:38 +0800

    added if elif on food details

--- app/src/main/java/com/example/foodpapa2021/FoodDetail.java
+++ app/src/main/java/com/example/foodpapa2021/FoodDetail.java
@@ -36,98 +36,158 @@ public class FoodDetail extends AppCompatActivity {
     Button food_detail_updateBasket;
 
     @AfterViews
-    public void init(){
+    public void init() {
         food_detail_quantity.setText(String.valueOf(quantity));
     }
 
     @Click
-    public void food_detail_add(){
+    public void food_detail_add() {
         quantity++;
         food_detail_quantity.setText(String.valueOf(quantity));
     }
 
     @Click
-    public void food_detail_subtract(){
+    public void food_detail_subtract() {
         quantity--;
-        if(quantity>0)
-        {
+        if (quantity > 0) {
             food_detail_quantity.setText(String.valueOf(quantity));
-        }
-        else if(quantity < 0){
+        } else if (quantity < 0) {
             quantity = 0;
             food_detail_quantity.setText(String.valueOf(quantity));
         }
     }
 
     @Click
-    public void food_detail_updateBasket(){
+    public void food_detail_updateBasket() {
 
         // fast food
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_jollibee_chicken);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_jollibee_chicwdrink);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_jollibee_fries);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_jollibee_sundae);
+//        if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_jollibee_chicken);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_jollibee_chicwdrink);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_jollibee_fries);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_jollibee_sundae);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_kfc_chicken);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_kfc_chicwdrink);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_kfc_fries);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_kfc_sundae);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_mcdo_chicken);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_mcdo_chicwdrink);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_mcdo_fries);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_mcdo_sundae);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_popeyes_chicken);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_popeyes_fries);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_popeyes_sundae);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_wendys_chicwdrink);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_wendys_fries);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_wendys_sundae);
+//        }
+//
+//        // milk tea
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tigersugar_black);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tigersugar_sugar);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_coco_black);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_coco_classic);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_coco_pearl);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_coco_sugar);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_gongcha_black);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_gongcha_classic);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_gongcha_pearl);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_gongcha_sugar);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_macao_classic);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_macao_pearl);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_macao_sugar);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_serenitea_classic);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_serenitea_pearl);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_serenitea_sugar);
+//        }
+//
+//        // casual
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_italiannis_salad);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_itallianis_pasta);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_itallianis_ribs);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_icedtea);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_outback_pasta);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_outback_ribs);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_outback_salad);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_racks_pasta);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_racks_ribs);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_racks_salad);
+//        }
+//
+//        else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tgif_icedtea);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tgif_pasta);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tgif_ribs);
+//        } else if () {
+//            food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tgif_salad);
+//        }
 
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_kfc_chicken);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_kfc_chicwdrink);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_kfc_fries);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_kfc_sundae);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_mcdo_chicken);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_mcdo_chicwdrink);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_mcdo_fries);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_mcdo_sundae);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_popeyes_chicken);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_popeyes_fries);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_popeyes_sundae);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_wendys_chicwdrink);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_wendys_fries);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_wendys_sundae);
-
-        // milk tea
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tigersugar_black);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tigersugar_sugar);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_coco_black);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_coco_classic);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_coco_pearl);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_coco_sugar);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_gongcha_black);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_gongcha_classic);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_gongcha_pearl);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_gongcha_sugar);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_macao_classic);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_macao_pearl);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_macao_sugar);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_serenitea_classic);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_serenitea_pearl);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_serenitea_sugar);
-
-        // casual
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_italiannis_salad);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_itallianis_pasta);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_itallianis_ribs);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_icedtea);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_outback_pasta);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_outback_ribs);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_outback_salad);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_racks_pasta);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_racks_ribs);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_racks_salad);
-
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tgif_icedtea);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tgif_pasta);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tgif_ribs);
-        food_detail_foodimg.setImageResource(R.drawable.foodlist_img_tgif_salad);
     }
-
 }
