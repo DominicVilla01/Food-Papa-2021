899d4d80c9e58958bed611fa48c8d0ea92733970
DominicVilla01 <dominic.villa@obf.ateneo.edu>
2021-08-07 00:58:42 +0800

    Add changes to Dani/feature

--- app/src/main/java/com/example/foodpapa2021/OrderPage.java
+++ app/src/main/java/com/example/foodpapa2021/OrderPage.java
@@ -2,6 +2,7 @@ package com.example.foodpapa2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -11,19 +12,16 @@ import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_order_page)
-public class OrderPage<adapter> extends AppCompatActivity
-{
-//    private static final String[] locations = new String[]{"Paranaque, Makati, Quezon City"};
-
-    String[] locations = getResources().getStringArray(R.array.locations);
+public class OrderPage extends AppCompatActivity {
 
     @ViewById
     AutoCompleteTextView order_page_location;
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, locations);
 
     @AfterViews
-    public AutoCompleteTextView getOrder_page_location() {
+    public void init(){
+        String[] locations = getResources().getStringArray(R.array.locations);
+        @SuppressLint("ResourceType") ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,locations);
         order_page_location.setAdapter(adapter);
-        return order_page_location;
+
     }
 }
