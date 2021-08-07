533fafc22675df3bb39df19e8f3a3ea57d2dfb1b
alyssago <denise.go@obf.ateneo.edu>
2021-08-07 21:24:26 +0800

    initial milktea menu adapter

---
+++ app/src/main/java/com/example/foodpapa2021/adapters/MilkteaMenu.java
@@ -0,0 +1,95 @@
+package com.example.foodpapa2021.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodpapa2021.FoodDetail;
+import com.example.foodpapa2021.R;
+import com.example.foodpapa2021.RestaurantPage;
+import com.example.foodpapa2021.realm.CategoryList;
+import com.example.foodpapa2021.realm.FoodList;
+import com.example.foodpapa2021.realm.FoodList_m;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
+
+public class MilkteaMenu extends RealmRecyclerViewAdapter<FoodList_m, FastFoodMenu.ViewHolder> {
+
+    FoodDetail activity;
+
+    public FastFoodMenu(FoodDetail activity, @Nullable OrderedRealmCollection<FoodList_m> data, boolean autoUpdate) {
+        super(data, autoUpdate);
+        this.activity = activity;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView food_obj_name;
+        TextView food_obj_price;
+        ImageView food_img_img;
+
+        public ViewHolder(@Nonnull View food_ffView){
+            super (food_ffView);
+
+            price = categoryView.findViewById(R.id.food_obj_price);
+            name = categoryView.findViewById(R.id.food_obj_name);
+            img = categoryView.findViewById(R.id.food_img_img);
+        }
+    }
+
+    @Nonnull
+    @Override
+    public FastFoodMenu.ViewHolder onCreateViewHolder (@Nonnull ViewGroup parent, int viewType){
+        View v = activity.getLayoutInflater().inflate(R.layout.category_objects, parent, false);
+
+        FastFoodMenu.ViewHolder vh = new FastFoodMenu.ViewHolder(v);
+        return vh
+    }
+
+    @Override
+    public void onBindViewHolder(@Nonnull FastFoodMenu.ViewHolder holder, int position){
+        FoodList_m cat = getItem(position);
+
+        holder.name.setText(ff.getFf_name());
+        holder.price.setText(ff.getFf_price());
+
+        if (ff.getFf_name().equals("Coco"))
+        {
+            holder.img.setImageResource(R.drawable.coco_logo);
+        }
+        else if (ff.getFf_name().equals("Gongcha"))
+        {
+            holder.img.setImageResource(R.drawable.gongcha_logo);
+        }
+        else if (ff.getFf_name().equals("Macao Imperial"))
+        {
+            holder.img.setImageResource(R.drawable.macao_logo);
+        }
+        else if (ff.getFf_name().equals("Serenitea"))
+        {
+            holder.img.setImageResource(R.drawable.serenitea_logo);
+        }
+        else if (ff.getFf_name().equals("Tiger Sugar"))
+        {
+            holder.img.setImageResource(R.drawable.tigersugar_logo);
+        }
+
+        holder.ffobj.setTag(ff);
+        holder.ffobj.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                activity.ffObj((FoodList_m) v.getTag());
+            }
+        });
+    }
+}
+
+
+
+
