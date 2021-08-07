a9f093084cba2e389447f6bfbe931226315b6fd8
GDivino <68019100+GDivino@users.noreply.github.com>
2021-08-07 18:01:54 +0800

    Added Fast casual adapter and realm

--- app/src/main/java/com/example/foodpapa2021/RestaurantPage.java
+++ app/src/main/java/com/example/foodpapa2021/RestaurantPage.java
@@ -1,12 +1,49 @@
 package com.example.foodpapa2021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.example.foodpapa2021.adapters.FastCasualMenu;
+import com.example.foodpapa2021.realm.FoodList_fc;
+import com.example.foodpapa2021.realm.RestaurantList;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 @EActivity(R.layout.activity_restaurant_page)
 public class RestaurantPage extends AppCompatActivity {
+    SharedPreferences prefs;
+    Realm realm;
 
+    @ViewById
+    RecyclerView res_page_foodlist;
+
+    @AfterViews
+    public void init() {
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        res_page_foodlist.setLayoutManager(mLayoutManager);
+
+        RealmResults<FoodList_fc> list = realm.where(FoodList_fc.class).findAll();
+        FastCasualMenu fcAdapter = new FastCasualMenu(this, list, true);
+        res_page_foodlist.setAdapter(fcAdapter);
+    }
+
+    public void fcObj(FoodList_fc fc)
+    {
+        prefs = this.getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString("fc_uuid", fc.getUuid());
+        edit.apply();
+
+        FoodDetail_.intent(this).start();
+    }
 }
